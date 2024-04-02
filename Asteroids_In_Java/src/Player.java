eb62f8b705399b89cc94a5a289a09af0ca922ced
JasonZigmant <162187832+JasonZigmant@users.noreply.github.com>
2024-04-01 20:37:36 -0500

    Update Player.java

--- Asteroids_In_Java/src/Player.java
+++ Asteroids_In_Java/src/Player.java
@@ -1,3 +1,17 @@
 public class Player {
+  private double x; // meters
+  private double y; // meters
+  private double radius; // meters
+  private double xSpeed; // meters/second
+  private double ySpeed; // meters/second
+  private HitBox hitbox; // Collision detection tool
 
+   public Player(double x, double y, double radius, double xSpeed, double ySpeed, HitBox hitbox) {
+      this.x = x; // meters
+      this.y = y; // meters
+      this.radius = radius; //meters
+      this.xSpeed = xSpeed; // meters/second
+      this.ySpeed = ySpeed; // meter/second
+      this.hitbox = hitbox; // Collision-detection tool
+   }
 }
