fa54464a175770bc90074f0e2e36decb66f5bce5
ImNotStupidBro <c.blair@marquette.edu>
2024-04-03 10:44:12 -0500

    Added constructive comments to HitBox.java

--- Asteroids_In_Java/src/HitBox.java
+++ Asteroids_In_Java/src/HitBox.java
@@ -1,6 +1,8 @@
 /*
 This class was made with help from this website:
 https://www.stefanonsoftware.com/post/gamedev-hit-detection
+
+@author Christian Blair
 */
 public class HitBox{
    private double x, y, width, height;
@@ -15,14 +17,18 @@ public class HitBox{
       x += dx;
       y += dy;
    }
-   
+   /**
+   * Set the hitboxes parameters once declared.
+   */
    public void set(double x, double y, double width, double height){
       this.x = x;
       this.y = y;
       this.width = width;
       this.height = height;
    }
-   
+   /**
+   * Check whether or not the hitboxes of one object collides with another.
+   */
    public boolean intersects(HitBox h) {
       return x + width >= h.x && h.x + h.width >= x && y + height >= h.y && h.y + h.height >= y;
    }
