9b4d63a8bb69e6dbad9170ab9f49439e973b9b83
ImNotStupidBro <c.blair@marquette.edu>
2024-04-15 14:31:48 -0500

    Asteroid Drawing Rework in progress
    I have a new strategy to draw the asteroids now. Check the "Asteroid" java file for more details.

--- Asteroids_In_Java/src/Asteroid.java
+++ Asteroids_In_Java/src/Asteroid.java
@@ -4,61 +4,30 @@ import javafx.scene.shape.Polygon;
 import java.util.Random;
 public class Asteroid extends InteractableObject{
 
-   private static double verticeCoordinates[];
+   private double[] XVertices;
+   private double[] YVertices;
    
-   public Asteroid(double x, double y, Polygon theShape, HitBox hitbox, double[] VCoords){
+   public Asteroid(double x, double y, Polygon theShape, HitBox hitbox){
       super(x, y, theShape, hitbox);
-      verticeCoordinates = VCoords;
    }
    
-   private static double[] generateConfiguration() {
+   private void generateConfiguration(double X, double Y, int SCALE) {
       Random rand = new Random();
       int randNum = rand.nextInt(2);
 
       if(randNum == 0){
-         return new double[]{
-         40.0, 46.0, 
-         46.0, 40.0,
-         52.0, 40.0,
-         56.0, 46.0,
-         56.0, 52.0,
-         52.0, 56.0,
-         48.0, 56.0,
-         48.0, 50.0,
-         44.0, 56.0,
-         40.0, 50.0,
-         44.0, 48.0,
-         40.0, 46.0};
+      //New Strategy: Create templates for each configuration by referencing the position of the asteroid
+      // and then drawing the vertices from it according to each vertice's disposition.
+         XVertices = new double[]{X + (4.0 * SCALE), X + (4.0 * SCALE), X + (4.0 * SCALE), X + (4.0 * SCALE), 
+         X + (4.0 * SCALE), X + (4.0 * SCALE), X + (4.0 * SCALE), X + (4.0 * SCALE), 
+         X + (4.0 * SCALE), X + (4.0 * SCALE), X + (4.0 * SCALE), X + (4.0 * SCALE)};
+         YVertices = new double[]{Y + (4.0 * SCALE), 40.0, 40.0, 46.0, 52.0, 56.0, 56.0, 50.0, 56.0, 50.0, 48.0, 46.0};
       }else if(randNum == 1){
-         return new double[]{
-         60.0, 46.0, 
-         64.0, 40.0,
-         68.0, 42.0,
-         72.0, 40.0,
-         76.0, 44.0,
-         72.0, 46.0,
-         76.0, 50.0,
-         72.0, 56.0,
-         66.0, 54.0,
-         64.0, 56.0,
-         60.0, 52.0,
-         62.0, 48.0,
-         60.0, 44.0};
+         XVertices = new double[]{60, 64, 68, 72, 76, 72, 76, 72, 66, 64, 60, 62, 60};
+         YVertices = new double[]{46.0, 40.0, 40.0, 46.0, 52.0, 56.0, 56.0, 50.0, 56.0, 50.0, 48.0, 46.0};
       }else{
-         return new double[] {
-         80.0, 44.0, 
-         86.0, 44.0,
-         84.0, 40.0,
-         90.0, 40.0,
-         96.0, 44.0,
-         96.0, 46.0,
-         90.0, 48.0,
-         96.0, 50.0,
-         92.0, 56.0,
-         90.0, 54.0,
-         82.0, 56.0,
-         80.0, 50.0,
-         80.0, 44.0};
+         XVertices = new double[]{40.0, 46.0, 52.0, 56.0, 56.0, 52.0, 48.0, 48.0, 44.0, 40.0, 44.0, 40.0};
+         YVertices = new double[]{46.0, 40.0, 40.0, 46.0, 52.0, 56.0, 56.0, 50.0, 56.0, 50.0, 48.0, 46.0};
       } 
    }
    
@@ -70,11 +39,11 @@ public class Asteroid extends InteractableObject{
       Random rand = new Random();
       int asteroidXStartLocation = rand.nextInt(World.X_DIMENSION);
       int asteroidYStartLocation = rand.nextInt(World.Y_DIMENSION);
-      double[] asteroidVCoords = generateConfiguration();
       Polygon asteroidPolygon = generatePolygon(asteroidVCoords);
       Bounds asteroidBox = asteroidPolygon.getBoundsInParent();
       HitBox asteroidHitbox = new HitBox(asteroidXStartLocation, asteroidYStartLocation, asteroidBox.getWidth(), asteroidBox.getHeight());
-      Asteroid asteroid = new Asteroid(asteroidXStartLocation, asteroidYStartLocation, asteroidPolygon, asteroidHitbox, asteroidVCoords);
+      Asteroid asteroid = new Asteroid(asteroidXStartLocation, asteroidYStartLocation, asteroidPolygon, asteroidHitbox);
+      asteroid.generateConfiguration();
       return asteroid;
    }
    
